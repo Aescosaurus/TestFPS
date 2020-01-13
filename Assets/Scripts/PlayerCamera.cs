352bfe35f0796b711c79189cbfe5df8cadebed23
Aescosaurus <michaelbuslik@hotmail.com>
2020-01-13 10:56:46 -0800

    Project start.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerCamera
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		cam = transform.Find( "Main Camera" );
+		Assert.IsNotNull( cam );
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.Escape ) )
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+		if( Input.GetMouseButtonDown( 0 ) )
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		var aim = new Vector2( Input.GetAxis( "Mouse X" ),
+			Input.GetAxis( "Mouse Y" ) );
+
+		cam.transform.eulerAngles = new Vector3(
+			cam.eulerAngles.x - aim.y * rotationSpeed * Time.deltaTime,
+			cam.eulerAngles.y + aim.x * rotationSpeed * Time.deltaTime,
+			cam.eulerAngles.z );
+	}
+
+	Transform cam;
+
+	[SerializeField] float rotationSpeed = 5.0f;
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerMove
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		body = GetComponent<Rigidbody>();
+		Assert.IsNotNull( body );
+		cam = transform.Find( "Main Camera" );
+		Assert.IsNotNull( cam );
+		boxColl = GetComponent<BoxCollider>();
+	}
+
+	void Update()
+	{
+		moveInput.x = Input.GetAxis( "Horizontal" );
+		moveInput.y = Input.GetAxis( "Vertical" );
+
+		var footOverlaps = Physics.OverlapBox( boxColl.bounds.center,
+			boxColl.bounds.extents );
+		bool touchingGround = false;
+		foreach( var footOverlap in footOverlaps )
+		{
+			if( footOverlap.gameObject.layer != LayerMask
+				.NameToLayer( "Player" ) )
+			{
+				touchingGround = true;
+				break;
+			}
+		}
+		
+		jumping = false;
+		if( Input.GetKey( KeyCode.Space ) && touchingGround )
+		{
+			jumping = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		var forward = cam.forward;
+		forward.y = 0.0f;
+
+		var xMove = cam.right * moveInput.x;
+		var yMove = forward * moveInput.y;
+		var move = ( xMove + yMove ).normalized;
+		move.y = 0.0f;
+
+		transform.Translate( move * moveSpeed * Time.fixedDeltaTime );
+
+		if( jumping )
+		{
+			var vel = body.velocity;
+			vel.y = 0.0f;
+			body.velocity = vel;
+			body.AddForce( Vector3.up * jumpPower,
+				ForceMode.Impulse );
+			jumping = false;
+		}
+	}
+
+	Rigidbody body;
+	Transform cam;
+	BoxCollider boxColl;
+
+	[SerializeField] float moveSpeed = 5.0f;
+	[SerializeField] float jumpPower = 1.0f;
+
+	Vector2 moveInput = Vector2.zero;
+	bool jumping = false;
+}
