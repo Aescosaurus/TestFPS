cb16cbf7f0160348a5bff9d840a1bc0de95253f8
Aescosaurus <michaelbuslik@hotmail.com>
2020-01-14 20:39:32 -0800

    Walking animation (whew).

---
+++ Assets/Scripts/AI/AIWalk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWalk
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		// GetComponent<Animator>().SetTrigger( "Walk" );
+	}
+
+	void Update()
+	{
+		if( !walkDuration.Update( Time.deltaTime ) )
+		{
+			transform.Translate( Vector3.forward * speed *
+				Time.deltaTime );
+		}
+		else
+		{
+			walkDuration.Reset();
+			transform.Rotate( Vector3.up,180.0f );
+		}
+	}
+
+	[SerializeField] float speed = 10.0f;
+	[SerializeField] Timer walkDuration = new Timer( 5.0f );
+}
