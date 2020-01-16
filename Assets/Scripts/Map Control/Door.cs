0fbd301fd463357686a8f779ed868e87e7c32400
Aescosaurus <michaelbuslik@hotmail.com>
2020-01-15 22:41:29 -0800

    Building interior scene.

---
+++ Assets/Scripts/Map Control/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Door
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		Assert.IsTrue( !openable || levelName != "" );
+	}
+
+	[SerializeField] public bool openable = false;
+	[SerializeField] public string levelName = "";
+}
---
+++ Assets/Scripts/Player/DoorOpener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class DoorOpener
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		cam = Camera.main;
+		Assert.IsNotNull( cam );
+	}
+
+	void Update()
+	{
+		if( Input.GetAxis( "Interact" ) > 0.0f )
+		{
+			RaycastHit hit;
+			if( Physics.Raycast( cam.transform.position,
+				cam.transform.forward,out hit,range,
+				LayerMask.GetMask( "Door" ) ) )
+			{
+				var doorScr = hit.transform.GetComponent<Door>();
+				Assert.IsNotNull( doorScr );
+				if( doorScr.openable )
+				{
+					SceneManager.LoadScene( doorScr.levelName );
+				}
+			}
+		}
+	}
+
+	Camera cam;
+
+	[SerializeField] float range = 0.6f;
+}
